2035892f9b18a83c031247821c104ab2be1b75ad
Pivot <sulukeq1204@gmail.com>
2026-06-08 18:39:21 +0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,17 +2,18 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
-const connectDB = require('./db'); // import hàm kết nối DB
+const connectDB = require('./db');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Kết nối MongoDB Atlas
 connectDB();
 
 app.use(cors());
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/payments/webhook', express.raw({ type: '*/*' }));
+app.use(express.json());
 
 // Mount routes
 app.use('/api/workers', require('./src/routes/workers'));
